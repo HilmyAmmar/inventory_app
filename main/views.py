08a077b1e6ec2d514f5a41f20fda3b161def2bf5
HilmyAmmar <hilmyammardarmawan17@gmail.com>
2023-09-12 21:09:14 +0700

    revisi konten

--- main/views.py
+++ main/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'name': 'main component',
-        'amount': '100',
-        'description': 'Component that is used for main component in the product ',
-        'price': 'Rp 2.500.000',
+        'name': 'Piece of Eden',
+        'amount': '1',
+        'description': 'Technologically advanced pieces of equipment created by First Civilization',
+        'price': '$100.000.000.000.000',
         'date': 'Mon, 14 Jan 2023'
     }
 
